687793fa24cfb7d44dfb6449bde78a43660aa940
Yuborama <davidjohan2@hotmail.com>
2023-08-24 23:55:52 -0500

    refactor: clean home and import component game

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,6 @@
 "use client";
 
-import Image from "next/image";
-// import styles from "./page.module.css";
-import "./wordsearchComponente.css";
-
-import { useState, useEffect } from "react";
-import createGrid, { Ipoint } from "./createGrid";
-import { checkSelectedWord, getSelectedHoverData } from "./accions";
-import LetterComponent from "./components/Letter";
+import Game from "./components/Game/Index";
 
 const words = [
   "película",
@@ -31,109 +24,9 @@ const words = [
   "escenografía",
   "maquillaje",
 ];
-const creatgridWords = createGrid(words);
 
 const Home = () => {
-  const [grid, setGrid] = useState<string[][]>([]);
-  const [positionsSelected, setPositionsSelected] = useState<Ipoint[]>([]);
-  const [correctPositions, setCorrectPositions] = useState<Ipoint[]>([]);
-  const [selecting, setSelecting] = useState(false);
-  const [startPos, setStartPos] = useState({ row: -1, col: -1 });
-  const [currentPos, setCurrentPos] = useState({ row: -1, col: -1 });
-
-  const clearSelection = () => {
-    setPositionsSelected([]);
-    setCurrentPos({ row: -1, col: -1 });
-    setStartPos({ row: -1, col: -1 });
-  };
-
-  const addPosition = (row: number, col: number) => {
-    setPositionsSelected((prev) => [...prev, { row, col }]);
-  };
-
-  const handleCellMouseDown = (row: number, col: number) => {
-    setSelecting(true);
-    setStartPos({ row, col });
-    setCurrentPos({ row, col });
-    addPosition(row, col);
-  };
-
-  const handleCellMouseEnter = (row: number, col: number) => {
-    if (!selecting) return;
-    const { selectedPositions } = getSelectedHoverData(grid, startPos, {
-      row,
-      col,
-    });
-    setPositionsSelected(selectedPositions);
-    setCurrentPos({ row, col });
-  };
-
-  const handleCellMouseUp = () => {
-    if (!selecting) return;
-    setSelecting(false);
-    const { selectedLetters, selectedPositions } = getSelectedHoverData(
-      grid,
-      startPos,
-      currentPos
-    );
-    const isCorrect = checkSelectedWord(selectedLetters.join(""), words);
-    setCorrectPositions((prev) =>
-      isCorrect ? [...prev, ...selectedPositions] : prev
-    );
-    // reset states
-    clearSelection();
-  };
-
-  useEffect(() => {
-    setGrid(creatgridWords);
-  }, []);
-  return (
-    <div>
-      <h1>Word Search Game</h1>
-      <div className="grid-container">
-        <>
-          {grid.map((row, rowIndex) => (
-            <div key={rowIndex} className="grid-row">
-              {row.map((letter, columnIndex) => (
-                <div
-                  key={`${rowIndex}, ${columnIndex}`}
-                  onMouseDown={() => handleCellMouseDown(rowIndex, columnIndex)}
-                  onMouseEnter={() =>
-                    handleCellMouseEnter(rowIndex, columnIndex)
-                  }
-                  onMouseUp={handleCellMouseUp}
-                  className={`grid-cell ${
-                    positionsSelected.some(
-                      (pos) => pos.row === rowIndex && pos.col === columnIndex
-                    )
-                      ? "selected"
-                      : ""
-                  }
-                  ${
-                    correctPositions.some(
-                      (pos) => pos.row === rowIndex && pos.col === columnIndex
-                    )
-                      ? "correct"
-                      : ""
-                  }`}
-                >
-                  <LetterComponent letter={letter} />
-                </div>
-              ))}
-            </div>
-          ))}
-        </>
-      </div>
-      <div className="word-list">
-        <h2>Word List</h2>
-        <ul>
-          {words.map((word, index) => (
-            <li key={index}>{word}</li>
-          ))}
-        </ul>
-      </div>
-    </div>
-  );
+  return <Game words={words} />;
 };
 
 export default Home;
